a6bd17ac532030a6a5c71f35c29b6e7d3d340a25
Andres Apodaca <andresapodaca38@gmail.com>
2015-10-12 19:54:41 -0600

    modificacion en precProtectores.php

--- precProtectores.php
+++ precProtectores.php
@@ -15,7 +15,7 @@ $result = $conn->query($sql);
 <!DOCTYPE html>
 
 <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
